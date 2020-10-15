1eca86e06df8e21e3cb1fc14738e95d32d64929b
marcos <marcos.rib.sousa@gmail.com>
2020-10-15 19:32:41 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors'
 import './database/connection';
 import path from  'path';
 
@@ -8,6 +9,7 @@ import errorHandler from './errors/handler';
 import routes from './routes'
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
